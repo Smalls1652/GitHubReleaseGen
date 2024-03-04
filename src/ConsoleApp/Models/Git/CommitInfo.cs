4dadc323c31b8c6f4c826b2329be34d2d9eea243
Timothy Small <smalls.online@icloud.com>
2024-03-04 12:39:26 -0500

    Change how refs are handled (#6)
    * Add CommitInfo class
    
    * Add GitCliException
    
    * Add GetCommitInfoAsync method
    
    * Handle getting git commit info directly in CommitInfo class
    
    * Add CommitsCollection class for handling getting commits between two refs
    
    * Remove GitCliUtils
    
    * Switch to using the new CommitInfo and CommitsCollection classes

---
+++ src/ConsoleApp/Models/Git/CommitInfo.cs
@@ -0,0 +1,139 @@
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+
+namespace GitHubReleaseGen.ConsoleApp.Models.Git;
+
+/// <summary>
+/// Represents information about a commit in a Git repository.
+/// </summary>
+public sealed partial class CommitInfo
+{
+    private readonly string _inputRef;
+    private readonly string? _repoPath;
+
+    private string? _shaAbbreviated;
+    private string? _refName;
+    private string? _subject;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="CommitInfo"/> class.
+    /// </summary>
+    /// <param name="inputRef">The ref to get the commit information for.</param>
+    public CommitInfo(string inputRef) : this(inputRef, null) { }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="CommitInfo"/> class.
+    /// </summary>
+    /// <param name="inputRef">The ref to get the commit information for.</param>
+    /// <param name="repoPath">The path to the local repository.</param>
+    public CommitInfo(string inputRef, string? repoPath)
+    {
+        _inputRef = inputRef;
+        _repoPath = repoPath;
+    }
+
+    /// <summary>
+    /// The abbreviated SHA of the commit.
+    /// </summary>
+    public string ShaAbbreviated
+    {
+        get => _shaAbbreviated ?? throw new NullReferenceException("Data has not been retrieved yet.");
+    }
+
+    /// <summary>
+    /// The name of the ref.
+    /// </summary>
+    public string RefName
+    {
+        get => _refName ?? throw new NullReferenceException("Data has not been retrieved yet.");
+    }
+
+    /// <summary>
+    /// The subject of the commit.
+    /// </summary>
+    public string Subject
+    {
+        get => _subject ?? throw new NullReferenceException("Data has not been retrieved yet.");
+    }
+
+    /// <summary>
+    /// Initializes the <see cref="CommitInfo"/> instance.
+    /// </summary>
+    /// <returns></returns>
+    /// <exception cref="InvalidOperationException">The git log output could not be parsed.</exception>
+    public async Task GetCommitInfoAsync()
+    {
+        ProcessStartInfo processStartInfo = CreateGitProcessStartInfo(
+            arguments: [
+                "log",
+                "--no-patch",
+                "--format='%h - %S - %s'",
+                _inputRef
+            ],
+            workingDirectory: _repoPath ?? Environment.CurrentDirectory
+        );
+
+        using Process process = await RunGitProcessAsync(processStartInfo);
+
+        string output = await process.StandardOutput.ReadToEndAsync();
+
+        var match = CommitLogRegex().Match(output);
+
+        if (!match.Success)
+        {
+            throw new InvalidOperationException("The git log output could not be parsed.");
+        }
+
+        _shaAbbreviated = match.Groups["sha"].Value;
+        _refName = match.Groups["refName"].Value;
+        _subject = match.Groups["subject"].Value;
+    }
+
+    /// <summary>
+    /// Creates a new <see cref="ProcessStartInfo"/> instance for running a 'git' command.
+    /// </summary>
+    /// <param name="arguments">Arguments to pass to the 'git' command.</param>
+    /// <param name="workingDirectory">The directory to run the 'git' command in.</param>
+    /// <returns>A new <see cref="ProcessStartInfo"/> instance.</returns>
+    private static ProcessStartInfo CreateGitProcessStartInfo(string[] arguments, string? workingDirectory)
+    {
+        return new(
+            fileName: "git",
+            arguments: arguments
+        )
+        {
+            CreateNoWindow = true,
+            RedirectStandardOutput = true,
+            RedirectStandardError = true,
+            WorkingDirectory = workingDirectory ?? Environment.CurrentDirectory
+        };
+    }
+
+    /// <summary>
+    /// Runs a 'git' command.
+    /// </summary>
+    /// <param name="processStartInfo">The <see cref="ProcessStartInfo"/> instance to use for running the 'git' command.</param>
+    /// <returns>An instance of <see cref="Process"/> representing the running 'git' command.</returns>
+    /// <exception cref="InvalidOperationException">Failed to start git process.</exception>
+    /// <exception cref="GitCliException">An error occurred while running the 'git' command.</exception>
+    private static async Task<Process> RunGitProcessAsync(ProcessStartInfo processStartInfo)
+    {
+        Process process = Process.Start(processStartInfo) ?? throw new InvalidOperationException("Failed to start git process.");
+
+        await process.WaitForExitAsync();
+
+        if (process.ExitCode != 0)
+        {
+            string errorOutput = await process.StandardError.ReadToEndAsync();
+
+            throw new GitCliException("An error occurred while running the 'git' command.", errorOutput, GitCliExceptionType.Unknown);
+        }
+
+        return process;
+    }
+
+    [GeneratedRegex(
+        pattern: "(?'sha'[a-zA-z0-9]{7}) - (?'refName'.+?) - (?'subject'.+)"
+    )]
+    internal static partial Regex CommitLogRegex();
+}
---
+++ src/ConsoleApp/Models/Git/CommitsCollection.cs
@@ -0,0 +1,146 @@
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+
+namespace GitHubReleaseGen.ConsoleApp.Models.Git;
+
+/// <summary>
+/// A collection of commits in a Git repository.
+/// </summary>
+public sealed partial class CommitsCollection
+{
+    private readonly CommitInfo? _baseCommitRef;
+    private readonly CommitInfo? _newCommitRef;
+    private readonly string? _repoPath;
+
+    private List<string>? _commits;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="CommitsCollection"/> class.
+    /// </summary>
+    /// <param name="baseCommitRef">The base commit ref.</param>
+    /// <param name="newCommitRef">The new commit ref.</param>
+    public CommitsCollection(CommitInfo baseCommitRef, CommitInfo newCommitRef) : this(baseCommitRef, newCommitRef, null) { }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="CommitsCollection"/> class.
+    /// </summary>
+    /// <param name="baseCommitRef">The base commit ref.</param>
+    /// <param name="newCommitRef">The new commit ref.</param>
+    /// <param name="repoPath">The path to the local repository.</param>
+    public CommitsCollection(CommitInfo baseCommitRef, CommitInfo newCommitRef, string? repoPath)
+    {
+        _baseCommitRef = baseCommitRef;
+        _newCommitRef = newCommitRef;
+        _repoPath = repoPath;
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="CommitsCollection"/> class.
+    /// </summary>
+    /// <param name="commits">An array of commits.</param>
+    public CommitsCollection(string[] commits)
+    {
+        _commits = new(commits);
+    }
+
+    /// <summary>
+    /// A collection of commit log entries.
+    /// </summary>
+    public List<string> Commits
+    {
+        get => _commits ?? throw new NullReferenceException("Data has not been retrieved yet.");
+    }
+
+    /// <summary>
+    /// Gets the commits between the base and new commit refs.
+    /// </summary>
+    /// <returns></returns>
+    /// <exception cref="NullReferenceException">A base or new commit ref was not set.</exception>
+    public async Task GetCommitsBetweenRefsAsync()
+    {
+        if (_baseCommitRef is null)
+        {
+            throw new NullReferenceException("Base commit ref is null.");
+        }
+
+        if (_newCommitRef is null)
+        {
+            throw new NullReferenceException("New commit ref is null.");
+        }
+
+        ProcessStartInfo processStartInfo = CreateGitProcessStartInfo(
+            arguments: [
+                "log",
+                $"{_baseCommitRef.RefName}..{_newCommitRef.RefName}",
+                "--reverse",
+                "--oneline"
+            ],
+            workingDirectory: _repoPath ?? Environment.CurrentDirectory
+        );
+
+        using Process process = await RunGitProcessAsync(processStartInfo);
+
+        string output = await process.StandardOutput.ReadToEndAsync();
+
+        if (_commits is null)
+        {
+            _commits = [];
+        }
+
+        foreach (Match matchItem in CommitRegex().Matches(output))
+        {
+            if (matchItem.Success && matchItem.Groups["commit"].Success)
+            {
+                _commits.Add(matchItem.Groups["commit"].Value);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Creates a new <see cref="ProcessStartInfo"/> instance for running a 'git' command.
+    /// </summary>
+    /// <param name="arguments">Arguments to pass to the 'git' command.</param>
+    /// <param name="workingDirectory">The directory to run the 'git' command in.</param>
+    /// <returns>A new <see cref="ProcessStartInfo"/> instance.</returns>
+    private static ProcessStartInfo CreateGitProcessStartInfo(string[] arguments, string? workingDirectory)
+    {
+        return new(
+            fileName: "git",
+            arguments: arguments
+        )
+        {
+            CreateNoWindow = true,
+            RedirectStandardOutput = true,
+            RedirectStandardError = true,
+            WorkingDirectory = workingDirectory ?? Environment.CurrentDirectory
+        };
+    }
+
+    /// <summary>
+    /// Runs a 'git' command.
+    /// </summary>
+    /// <param name="processStartInfo">The <see cref="ProcessStartInfo"/> instance to use for running the 'git' command.</param>
+    /// <returns>An instance of <see cref="Process"/> representing the running 'git' command.</returns>
+    /// <exception cref="InvalidOperationException">Failed to start git process.</exception>
+    /// <exception cref="GitCliException">An error occurred while running the 'git' command.</exception>
+    private static async Task<Process> RunGitProcessAsync(ProcessStartInfo processStartInfo)
+    {
+        Process process = Process.Start(processStartInfo) ?? throw new InvalidOperationException("Failed to start git process.");
+
+        await process.WaitForExitAsync();
+
+        if (process.ExitCode != 0)
+        {
+            string errorOutput = await process.StandardError.ReadToEndAsync();
+
+            throw new GitCliException("An error occurred while running the 'git' command.", errorOutput, GitCliExceptionType.Unknown);
+        }
+
+        return process;
+    }
+
+    [GeneratedRegex(
+        pattern: "(?'commit'[a-zA-z0-9]{7}) .+"
+    )]
+    internal static partial Regex CommitRegex();
+}
