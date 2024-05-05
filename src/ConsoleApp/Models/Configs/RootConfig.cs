1fd29d626cda3ab17805fb03dc8f1f8a71ce35eb
Timothy Small <smalls.online@icloud.com>
2024-05-05 15:49:05 -0400

    Add method for getting config data

--- src/ConsoleApp/Models/Configs/RootConfig.cs
+++ src/ConsoleApp/Models/Configs/RootConfig.cs
@@ -1,3 +1,5 @@
+using GitHubReleaseGen.ConsoleApp.Models.Git;
+
 namespace GitHubReleaseGen.ConsoleApp.Models.Configs;
 
 /// <summary>
@@ -9,4 +11,40 @@ public sealed class RootConfig
     /// Configuration options for the labels.
     /// </summary>
     public LabelsConfig Labels { get; set; } = new();
+
+    /// <summary>
+    /// Gets the configuration file.
+    /// </summary>
+    /// <returns>Data from the configuration file.</returns>
+    /// <exception cref="FileNotFoundException">The configuration file was not found.</exception>
+    /// <exception cref="InvalidOperationException">Failed to deserialize the configuration file.</exception>
+    public static async Task<RootConfig> GetConfigAsync() => await GetConfigAsync(Environment.CurrentDirectory);
+
+    /// <summary>
+    /// Gets the configuration file.
+    /// </summary>
+    /// <param name="workingDirectory">The working directory.</param>
+    /// <returns>Data from the configuration file.</returns>
+    /// <exception cref="FileNotFoundException">The configuration file was not found.</exception>
+    /// <exception cref="InvalidOperationException">Failed to deserialize the configuration file.</exception>
+    public static async Task<RootConfig> GetConfigAsync(string workingDirectory)
+    {
+        RootGitDirectory rootGitDirectory = new(workingDirectory);
+
+        string configPath = Path.Combine(rootGitDirectory.Path, ".gh-releasegen.json");
+
+        if (!File.Exists(configPath))
+        {
+            throw new FileNotFoundException("Configuration file not found.", configPath);
+        }
+
+        string json = await File.ReadAllTextAsync(configPath);
+
+        RootConfig config = JsonSerializer.Deserialize(
+            json: json,
+            jsonTypeInfo: ConfigJsonContext.Default.RootConfig
+        ) ?? throw new InvalidOperationException("Failed to deserialize the configuration file.");
+
+        return config;
+    }
 }
